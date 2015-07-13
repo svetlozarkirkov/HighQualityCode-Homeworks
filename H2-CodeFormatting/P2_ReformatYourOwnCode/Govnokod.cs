495c1bc138d0aa16aa12c7761b85ce692f03d539
Svetlozar Kirkov <svetlozark@gmail.com>
2015-07-13 14:12:18 +0300

    finished h2

---
+++ H2-CodeFormatting/P2_ReformatYourOwnCode/Govnokod.cs
@@ -0,0 +1,20 @@
+﻿namespace P2_ReformatYourOwnCode
+{
+    class Govnokod
+    {
+        //private static string RemoveInvalidChars(string source)
+        //{
+        //    foreach (var c in invalidChars)
+        //        source = source.Replace(c.ToString(), "");
+
+        //    return source;
+        //}
+
+        //public static string Validate(string source)
+        //{
+        //    source = RemoveInvalidChars(source);
+
+        //    return source;
+        //}
+    }
+}
---
+++ H2-CodeFormatting/P2_ReformatYourOwnCode/Reformatted.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace P2_ReformatYourOwnCode
+{
+    class Reformatted
+    {
+        private static string RemoveInvalidChars(string source, char[] invalidChars)
+        {
+            return invalidChars.Aggregate(source, (current, c) => current.Replace(c.ToString(), ""));
+        }
+
+        //Test
+        static void Main()
+        {
+            string text = "Once upon a time there was a fabulous kingdom!";
+            string processed = RemoveInvalidChars(text, new[]
+            {
+                's',
+                'c'
+            });
+
+            Console.WriteLine(processed);
+        }
+    }
+}
