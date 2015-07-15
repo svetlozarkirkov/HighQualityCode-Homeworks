821a9bec6541d68e120938d351a237ce814dfa4e
Svetlozar Kirkov <svetlozark@gmail.com>
2015-07-15 13:21:38 +0300

    h2 upd

---
+++ H2-CodeFormatting/P1_ReformatCode/Application.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_ReformatCode
+{
+    internal class Application
+    {
+        private static readonly StringBuilder output = new StringBuilder();
+        private static readonly EventHolder events = new EventHolder();
+
+        static void Main()
+        {
+            while (ExecuteNextCommand())
+            {
+            }
+            Console.WriteLine(output);
+        }
+
+        private static bool ExecuteNextCommand()
+        {
+            var command = Console.ReadLine();
+            if (command[0] == 'A')
+            {
+                AddEvent(command);
+                return true;
+            }
+            if (command[0] == 'D')
+            {
+                DeleteEvents(command);
+                return true;
+            }
+
+            if (command[0] == 'L')
+            {
+                ListEvents(command);
+                return true;
+            }
+            if (command[0] == 'E')
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private static void ListEvents(string command)
+        {
+            var pipeIndex = command.IndexOf('|')
+                ;
+            var date = GetDate(
+                command,
+                "ListEvents");
+            var countString = command.Substring(
+                pipeIndex + 1
+                );
+
+            var count = int.Parse(countString);
+            events.ListEvents(date,
+                count);
+        }
+
+        private static void DeleteEvents(string command)
+        {
+            var title = command.Substring
+                (
+                    "DeleteEvents".Length + 1
+                );
+
+            events.DeleteEvents(title);
+        }
+
+        private static void AddEvent(string command)
+        {
+            DateTime date;
+            string title;
+            string location;
+
+            GetParameters(command, "AddEvent", out date, out title, out location);
+
+            events.AddEvent(date, title, location);
+        }
+
+        private static void GetParameters
+            (string commandForExecution, string commandType, out DateTime dateAndTime,
+            out string eventTitle, out string eventLocation)
+        {
+            dateAndTime = GetDate(commandForExecution, commandType);
+            var firstPipeIndex = commandForExecution.IndexOf('|');
+            var lastPipeIndex = commandForExecution.LastIndexOf('|');
+
+            if (firstPipeIndex == lastPipeIndex)
+            {
+                eventTitle =
+                    commandForExecution.Substring(firstPipeIndex
+                                                  + 1).Trim();
+                eventLocation = "";
+            }
+            else
+            {
+                eventTitle = commandForExecution.Substring(
+                    firstPipeIndex + 1,
+                    lastPipeIndex - firstPipeIndex - 1)
+                    .Trim();
+                eventLocation = commandForExecution.Substring(lastPipeIndex + 1).Trim();
+            }
+        }
+
+        private static DateTime GetDate(string command, string commandType)
+        {
+            var date = DateTime.Parse(command.Substring(commandType.Length + 1, 20));
+            return date;
+        }
+    }
+}
---
+++ H2-CodeFormatting/P1_ReformatCode/EventHolder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_ReformatCode
+{
+    public class EventHolder
+    {
+        private readonly OrderedBag<Event> byDate = new OrderedBag<Event>();
+
+        private readonly MultiDictionary<string, Event> byTitle =
+            new MultiDictionary<string, Event>(true);
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            var newEvent = new Event(date,
+                title,
+                location);
+            byTitle.Add(
+                title.ToLower(),
+                newEvent
+                );
+            byDate.Add(newEvent);
+            Application.Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            var title = titleToDelete
+                .ToLower();
+            var removed = 0;
+            foreach (var eventToRemove in byTitle[title])
+            {
+                removed++;
+                byDate.Remove(eventToRemove);
+            }
+            byTitle.Remove(title);
+            Application.Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View
+                eventsToShow = byDate.RangeFrom(new Event(
+                    date, "", ""),
+                    true);
+            var showed = 0;
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count) break;
+                Application.Messages.PrintEvent(eventToShow);
+
+                showed++;
+            }
+            if (showed == 0) Application.Messages.NoEventsFound();
+        }
+    }
+}
